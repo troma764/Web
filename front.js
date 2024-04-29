51d3deff0ef9616cef6d3bde41f3d94ff5bddff4
Tim <troma764@gmail.com>
2024-04-29 11:36:44 +0200

    first commit

---
+++ front.js
@@ -0,0 +1,29 @@
+function LeapYear(year){
+    if (year % 4 == 0) {
+        console.log(year +  '  is a leap year')
+    }else{
+        console.log(year + '  is not a leap year')
+    }
+}
+
+LeapYear(2013);
+
+/*
+function checkLeapYear(year) {
+    if((0 == year % 4 ) && (0 != year % 100) || (0 == year % 400)) {
+        console.log(year + ' is a leap year');
+
+    }
+    else{
+        console.log(year + ' is not a leap year');
+    }
+
+}
+//const year = prompt('Enter a year:');
+checkLeapYear(2009);*/
+
+
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    
+}
